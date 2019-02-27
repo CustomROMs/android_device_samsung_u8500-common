6b727d9d8f69fff6f1469e231f7ec32ee791fce1
Shilin Victor <chrono.monochrome@gmail.com>
2019-02-27 14:16:54 +0300

    u8500-common: underp twrp-graphics

--- recovery/twrp-graphics.c
+++ recovery/twrp-graphics.c
@@ -714,7 +714,7 @@ void* gr_loadFont(const char* fontName)
     {
         char tmp[128];
 
-        sprintf(tmp, "/twres/fonts/%s.dat", fontName);
+        sprintf(tmp, "/res/fonts/%s.dat", fontName);
         fd = open(tmp, O_RDONLY);
         if (fd == -1)
             return NULL;
